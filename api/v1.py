bb76c199c7c116e5fe69ad4f4ba6d450b9126063
nik <nikshmakov@gmail.com>
2011-11-20 22:38:41 +0400

    delete fucking shame

--- api/v1.py
+++ api/v1.py
@@ -12,63 +12,6 @@ DT_HANDLER = lambda obj: obj.isoformat() if isinstance(obj, datetime.datetime) e
 
 api = Blueprint("api_v1", __name__)
 
-def get_user_id():
-    return guard.current_user.id
-
-
-def get_app_id():
-    return guard.current_app.id
-
-
-def get_remote_ip():
-    request.get('remote_addr', None)
-
-def from_json(obj):
-    try:
-        res = json.loads(obj)
-        if type(res) is not dict:
-            raise Exception()
-        return res
-    except Exception:
-        raise errors.InvalidJSONFormatError("Invalid json object \"%s\"" % obj)
-
-
-def extract_form_data():
-    """
-    Extracts form data when was passed json data in the HTTP headers
-    """
-    obj = request.form['data']
-    obj = from_json(obj)
-    return obj
-
-
-def extract_filter_opts():
-    """
-    Extracts filter data from the url
-    """
-    filter_opts = request.args.get('filter', None)
-    if filter_opts is not None:
-        filter_opts = filter_opts.strip()
-        filter_opts = from_json(filter_opts)
-        validate_on_forbidden_fields(filter_opts, int_fields.values())
-        if not len(filter_opts):
-            raise errors.InvalidRequestError('Invalid request syntax. There is no filters opts.')
-    
-    return filter_opts
-
-
-def is_force_mode():
-    force = request.args.get('force', None)
-    if force is None:
-        force = False
-    else:
-        force = force.strip()
-        if force == 'true':
-            force = True
-        else:
-            force = False
-    return force
-
 
 @api.route('/<bucket>', defaults={'key': None}, methods=['POST'])
 @api.route('/<bucket>/<key>', methods=['POST'])
@@ -77,12 +20,14 @@ def post_handler(bucket, key):
     """
     document = extract_form_data()
 
-    validate_on_forbidden_fields(document, int_fields.values())
+    validate(document)
 
     if key is not None:
         document[storage.ext_fields.KEY] = key
 
-    return _save_and_make_response(bucket, document)
+
+    document_key = storage.create(get_app_id(), get_user_id(), get_remote_ip(), document, bucket=bucket)
+    return jsonify({'response': {'key': document_key}})
 
 
 @api.route('/<bucket>/<keys:keys>', methods=['GET'])
@@ -123,7 +68,8 @@ def delete_by_keys_handler(bucket, keys):
         if not storage.is_document_exists(get_app_id(), get_user_id(), bucket, filter_opts):
             res.append({key: app.NOT_FOUND})
         else:
-            _delete(bucket, filter_opts)
+            storage.delete_by_filter(get_app_id(), get_user_id(), get_remote_ip(),
+                           bucket=bucket, filter_opts=filter_opts)
             res.append({key: app.OK})
     return jsonify({'response': res})
 
@@ -135,7 +81,10 @@ def delete_by_filter_handler(bucket):
     filter_opts = extract_filter_opts()
     if not storage.is_document_exists(get_app_id(), get_user_id(), bucket, filter_opts):
         return jsonify({'response': app.NOT_FOUND})
-    return _delete_and_make_response(bucket, filter_opts)
+
+    storage.delete_by_filter(get_app_id(), get_user_id(), get_remote_ip(),
+                           bucket=bucket, filter_opts=filter_opts)
+    return jsonify({'response': app.OK})
 
 
 @api.route('/<bucket>/<keys:keys>', methods=['PUT'])
@@ -148,16 +97,16 @@ def put_by_keys_handler(bucket, keys):
     document = extract_form_data()
     force = is_force_mode()
 
-    validate_on_forbidden_fields(document, int_fields.values())
+    validate(document)
 
     res = []
     for key in keys:
         filter_opts = {ext_fields.KEY: key}
 
-        if not _is_document_exists(bucket, filter_opts):
+        if not storage.is_document_exists(get_app_id(), get_user_id(), bucket, filter_opts):
             if force:
                 document[ext_fields.KEY] = key
-                _save(bucket, document)
+                storage.create(get_app_id(), get_user_id(), get_remote_ip(), document, bucket=bucket)
             res.append({key: app.NOT_FOUND})
         else:
             storage.update_by_key(get_app_id(), get_user_id(), get_remote_ip(),
@@ -175,11 +124,11 @@ def put_by_filter_handler(bucket):
     filter_opts = extract_filter_opts()
     force = is_force_mode()
 
-    validate_on_forbidden_fields(document, int_fields.values())
+    validate(document)
 
-    if not _is_document_exists(bucket, filter_opts):
+    if not storage.is_document_exists(get_app_id(), get_user_id(), bucket, filter_opts):
         if force:
-            key = _save(bucket, document)
+            key = storage.create(get_app_id(), get_user_id(), get_remote_ip(), document, bucket=bucket)
             return jsonify({'response': {ext_fields.KEY: key, STATUS_CODE: app.NOT_FOUND}})
         else:
             return jsonify({'response': {STATUS_CODE: app.NOT_FOUND}})
@@ -187,39 +136,6 @@ def put_by_filter_handler(bucket):
     storage.update_by_filter(get_app_id(), get_user_id(), get_remote_ip(),
                         bucket, document, filter_opts)
     return jsonify({'response': {STATUS_CODE: app.OK}})
-
-
-def validate_on_forbidden_fields(document, param):
-    fields = [key for key in document if key in param]
-    if len(fields):
-        raise errors.InvalidDocumentError(
-            errors.InvalidDocumentError.FORBIDDEN_FIELDS_MSG % ','.join(fields))
-    
-
-def _save(bucket, document):
-    # _save new entity and returns its key
-    document_key = storage.create(
-        get_app_id(), get_user_id(), get_remote_ip(), document, bucket=bucket
-    )
-    return document_key
-
-def _save_and_make_response(bucket, document):
-    # _save new entity and make response with returned key
-    document_key = _save(bucket, document)
-    return jsonify({'response': {'key': document_key}})
-
-
-def _delete(bucket, filter_opts=None):
-    storage.delete_by_filter(get_app_id(), get_user_id(), get_remote_ip(),
-                           bucket=bucket, filter_opts=filter_opts)
-
-def _delete_and_make_response(bucket, filter_opts=None):
-    _delete(bucket, filter_opts)
-    return jsonify({'response': app.OK})
-
-
-def _is_document_exists(bucket, criteria=None):
-    return storage.is_document_exists(get_app_id(), get_user_id(), bucket, criteria)
 
 
 @api.errorhandler(errors.DocumentNotFoundError)
@@ -271,4 +187,71 @@ def invalid_json_format(error):
     response_msg = json.dumps({'error': {STATUS_CODE: app.BAD_REQUEST,
                                          'message': error.message}})
     return response_msg, http.BAD_REQUEST
+
+
+def validate(document):
+    # assert that document not contains forbidden fields
+    fields = [key for key in document if key in int_fields.values()]
+    if len(fields):
+        raise errors.InvalidDocumentError(
+            errors.InvalidDocumentError.FORBIDDEN_FIELDS_MSG % ','.join(fields))
+
+    
+def get_user_id():
+    return guard.current_user.id
+
+
+def get_app_id():
+    return guard.current_app.id
+
+
+def get_remote_ip():
+    request.get('remote_addr', None)
+
+    
+def from_json(obj):
+    try:
+        res = json.loads(obj)
+        if type(res) is not dict:
+            raise Exception()
+        return res
+    except Exception:
+        raise errors.InvalidJSONFormatError("Invalid json object \"%s\"" % obj)
+
+
+def extract_form_data():
+    """
+    Extracts form data when was passed json data in the HTTP headers
+    """
+    obj = request.form['data']
+    obj = from_json(obj)
+    return obj
+
+
+def extract_filter_opts():
+    """
+    Extracts filter data from the url
+    """
+    filter_opts = request.args.get('filter', None)
+    if filter_opts is not None:
+        filter_opts = filter_opts.strip()
+        filter_opts = from_json(filter_opts)
+        validate(filter_opts)
+        if not len(filter_opts):
+            raise errors.InvalidRequestError('Invalid request syntax. There is no filters opts.')
+
+    return filter_opts
+
+
+def is_force_mode():
+    force = request.args.get('force', None)
+    if force is None:
+        force = False
+    else:
+        force = force.strip()
+        if force == 'true':
+            force = True
+        else:
+            force = False
+    return force
 
